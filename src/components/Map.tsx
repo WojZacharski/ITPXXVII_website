e999f681e923262bf1b078f9566eac251afad33c
WojZacharski <wojzacharski@student.agh.edu.pl>
2025-03-15 15:41:57 +0100

    Map update

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 //@ts-ignore
-import map_24 from "../images/map/map-25_final.svg";
+import map_24 from "../images/map/MapaITP.svg";
 //@ts-ignore
 import legend_24 from "../images/map/legend-25_new.svg";
 //@ts-ignore
@@ -14,31 +14,9 @@ const mapImages = importAll(
     require.context("../images/map", false, /\.(png|jpe?g|svg)$/)
 );
 
-const MapText = styled.p`
-    position: absolute;
-    margin-bottom: 1.5rem;
-    margin-top: 1.5rem;
-    top: 4%;
-    left: 50%;
-    transform: translateX(-50%);
-    color: #f08e17;
-    font-weight: bold;
-    font-size: clamp(1rem, 15vw, 4.5em);
-    letter-spacing: 0.03em;
-    width: auto;
-    max-width: 100vw; 
-    white-space: nowrap; 
-    text-align: center;
-
-    @media (max-width: 769px) {
-        font-size: clamp(1rem, 12vw, 3rem); 
-        margin-bottom: 1.5rem;
-        margin-top: 0.5rem;
-    }
-`;
-
 const Container = styled.div`
-    background-color: #f7e0cb;
+    color: #F0933B;
+    background-color: #F0933B;
     position: relative;
     display: flex;
     flex-direction: column;
@@ -46,7 +24,6 @@ const Container = styled.div`
     justify-content: center;
     width: 100%;
     min-height: 100vh;
-    padding: 2rem 0;
 
     @media (max-width: 769px) {
         //height: auto;
@@ -58,18 +35,58 @@ const Container = styled.div`
     }
 `;
 
-const MapImg = styled.img`
+const MapHeader = styled.div`
   position: absolute;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  top: 50%;
-  width: 80%;
+  top: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.8); 
+  padding: 1rem 0;
   z-index: 2;
-  @media (max-width: 769px) {
-      top: 35%;
-      width: 100%;
-      height: auto;
-  }
+    
+    @media (max-width: 769px) {
+        display: none;
+    }
+`;
+
+const MapText = styled.p`
+    color: #f08e17;
+    font-weight: bold;
+    font-size: clamp(1rem, 15vw, 4.5em);
+    letter-spacing: 0.03em;
+    white-space: nowrap;
+    text-align: center;
+    margin: 0;
+    padding: 0.5rem;
+    z-index: 2;
+
+    @media (max-width: 769px) {
+        display: none;
+    }
+`;
+
+
+const MapContainer = styled.div`
+    position: relative;
+    flex-grow: 1;
+    min-height: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    width: 100%;
+`;
+
+const MapImg = styled.img`
+    //position: absolute;
+    //top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    z-index: 0;
 `;
 
 const MapLegend = styled.img`
@@ -89,11 +106,16 @@ const MapLegend = styled.img`
 
 const Map: React.FC = () => {
   return (
+
       <Container id="map">
-        <MapText>Mapa wydarzenia</MapText>
-        <MapImg src={map_24} alt="map" />
-        <MapLegend src={legend_24} alt="list1" />
+          <MapHeader>
+              <MapText>Mapa wydarzenia</MapText>
+          </MapHeader>
+          <MapContainer>
+              <MapImg src={map_24} alt="map" />
+          </MapContainer>
       </Container>
+
   );
 };
 
